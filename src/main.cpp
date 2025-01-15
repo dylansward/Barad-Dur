f79789bafff1d504a4c4ef4a227c20385758fdd3
NikNikskywalker <nikolaihixon@gmail.com>
2025-01-14 20:20:11 -0500

    Added Robot Config

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,85 @@ competition Competition;
 
 // define your global instances of motors and other devices here
 
+vex::brain Brain;
+vex::controller con;
+
+vex::inertial imu(vex::PORT10, vex::turnType::right);
+vex::optical conveyor_optical(vex::PORT17);
+vex::triport expander(vex::PORT18);
+
+// Analog sensors
+CustomEncoder left_enc{Brain.ThreeWirePort.C, 2048};
+CustomEncoder right_enc{Brain.ThreeWirePort.E, 2048};
+CustomEncoder front_enc{Brain.ThreeWirePort.G, 2048};
+
+tracking_wheel_cfg_t left_enc_cfg{-0.0625, 3.625, M_PI, 1.0625};
+tracking_wheel_cfg_t right_enc_cfg{-0.0625, -3.625, 0, 1.0625};
+tracking_wheel_cfg_t front_enc_cfg{4.5, -0.375, (M_PI/2), 1.0625};
+
+// ================ OUTPUTS ================
+// Motors
+vex::motor left_front_top(vex::PORT8, vex::gearSetting::ratio6_1, true);
+vex::motor left_front_bottom(vex::PORT6, vex::gearSetting::ratio6_1, true);
+vex::motor left_back_top(vex::PORT7, vex::gearSetting::ratio6_1, false);
+vex::motor left_back_bottom(vex::PORT5, vex::gearSetting::ratio6_1, true);
+
+vex::motor right_front_top(vex::PORT3, vex::gearSetting::ratio6_1, false);
+vex::motor right_front_bottom(vex::PORT2, vex::gearSetting::ratio6_1, false);
+vex::motor right_back_top(vex::PORT1, vex::gearSetting::ratio6_1, true);
+vex::motor right_back_bottom(vex::PORT4, vex::gearSetting::ratio6_1, false);
+
+vex::motor_group left_motors{left_front_top, left_front_bottom, left_back_top, left_back_bottom};
+vex::motor_group right_motors{right_front_top, right_front_bottom, right_back_top, right_back_bottom};
+
+std::map<std::string, vex::motor &> motor_names{
+  {"left front top", left_front_top},   {"left front bottom", left_front_bottom},   {"left back top", left_back_top},   {"left back bottom", left_back_bottom},
+
+  {"right front top", right_front_top},   {"right front bottom", right_front_bottom},   {"right back top", right_back_top},   {"right back bottom", right_back_bottom},
+
+};
+
+// ================ SUBSYSTEMS ================
+
+PID::pid_config_t drive_pid_cfg{
+    .p = 0.045,
+    .i = 0.0,
+    .d = 0.0003,
+    .deadband = 0.5,
+    .on_target_time = 0.1,
+};
+
+PID drive_pid{drive_pid_cfg};
+
+PID::pid_config_t turn_pid_cfg{
+    .p = 0.01625,
+    .i = 0.005,
+    .d = 0.001,
+    //.deadband = 1.5,
+    .deadband = 2,
+    .on_target_time = 0.1,
+};
+
+PID turn_pid{turn_pid_cfg};
+
+PID::pid_config_t drive_correction_pid{};
+
+robot_specs_t robot_cfg{
+    .robot_radius = 12.0,
+    .odom_wheel_diam = 2.125,
+    .odom_gear_ratio = 1.0,
+    .dist_between_wheels = 12.5,
+
+    // .drive_correction_cutoff = 0,
+    .drive_feedback = &drive_pid,
+    .turn_feedback = &turn_pid,
+    .correction_pid = drive_correction_pid,
+};
+
+OdometryNWheel<3> odom({left_enc, right_enc, front_enc}, {left_enc_cfg, right_enc_cfg, front_enc_cfg}, &imu, true);
+// OdometryTank odom(left_enc, right_enc, robot_cfg, &imu);
+TankDrive drive_sys(left_motors, right_motors, robot_cfg, &odom);
+
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
 /*                                                                           */
@@ -27,7 +106,7 @@ competition Competition;
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-
+    imu.startCalibration();
     // All activities that occur before the competition starts
     // Example: clearing encoders, setting servo positions, ...
 }
